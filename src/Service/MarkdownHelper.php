e45974997babb67d22004a1eb8593738812f2218
Max <ageev.maks2014@gmail.com>
2019-02-02 12:24:59 +0500

    11-parameters

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -18,12 +18,17 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
+    /**
+     * @var bool
+     */
+    private $isDebug;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $logger;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source): string
@@ -31,6 +36,10 @@ class MarkdownHelper
         if (stripos($source, 'bacon') !== false) {
             $this->logger->info('They are talking about bacon again!');
         }
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
+        dump($this->cache);die;
 
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
